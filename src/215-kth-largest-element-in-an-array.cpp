32c744d788b220c56517ae454b13c2a6cd1e6661
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-06-20 19:53:44 +0800

    215. 数组中的第K个最大元素
    改用 size = k 的最小堆减少内存占用

--- src/215-kth-largest-element-in-an-array.cpp
+++ src/215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, less<int>> max_heap;
+        priority_queue<int, vector<int>, greater<int>> min_heap;
         for (int i : nums) {
-            max_heap.push(i);
+            if (min_heap.size() < k) {
+                min_heap.push(i);
+            } else if (i > min_heap.top()) {
+                min_heap.pop();
+                min_heap.push(i);
+            }
         }
-        for (int i = 0; i < k - 1; ++i) {
-            max_heap.pop();
-        }
-        return max_heap.top();
+        return min_heap.top();
     }
 };
