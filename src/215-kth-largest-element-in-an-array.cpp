1324bcccf12da0f187b9bd9dbb084293d9baed18
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-06-25 12:46:51 +0800

    215. 数组中的第K个最大元素
    优化逻辑

--- src/215-kth-largest-element-in-an-array.cpp
+++ src/215-kth-largest-element-in-an-array.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int, vector<int>, greater<int>> min_heap;
-        for (int i : nums) {
-            if (min_heap.size() < k) {
-                min_heap.push(i);
-            } else if (i > min_heap.top()) {
+        for (int i = 0; i < k; ++i) {
+            min_heap.push(nums[i]);
+        }
+        for (int i = k; i < nums.size(); ++i) {
+            if (nums[i] > min_heap.top()) {
                 min_heap.pop();
-                min_heap.push(i);
+                min_heap.push(nums[i]);
             }
         }
         return min_heap.top();
