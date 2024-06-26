d67df785eecbe940bfa9706c23187648a927c283
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-06-26 23:32:13 +0800

    19. 删除链表的倒数第 N 个结点
    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。

---
+++ src/19-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode sentinel(-1, head), *prev = &sentinel, *target = head,
+                                     *curr = head;
+        for (int i = 0; i < n; ++i) {
+            curr = curr->next;
+        }
+        while (curr) {
+            curr = curr->next;
+            target = target->next;
+            prev = prev->next;
+        }
+        if (target == head) {
+            return head->next;
+        } else {
+            prev->next = target->next;
+            return head;
+        }
+    }
+};
