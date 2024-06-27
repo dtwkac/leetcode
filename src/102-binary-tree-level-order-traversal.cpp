d353829957b622fd048efb121c914ad8611f2437
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-06-27 10:18:03 +0800

    102. 二叉树的层序遍历
    给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。

---
+++ src/102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> vv;
+        queue<TreeNode*> q;
+        if (root) {
+            q.push(root);
+        }
+        while (!q.empty()) {
+            vector<int> v;
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode* t = q.front();
+                v.push_back(t->val);
+                q.pop();
+                if (t->left) {
+                    q.push(t->left);
+                }
+                if (t->right) {
+                    q.push(t->right);
+                }
+            }
+            vv.push_back(v);
+        }
+        return vv;
+    }
+};
