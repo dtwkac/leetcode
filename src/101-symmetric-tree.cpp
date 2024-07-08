1f951da28a1c6926b5287fd3a79a376a3801fbe6
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-08 17:45:49 +0800

    101. 对称二叉树
    改用递归

--- src/101-symmetric-tree.cpp
+++ src/101-symmetric-tree.cpp
@@ -12,27 +12,21 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> v(size);
-            for (int i = 0; i < size; ++i) {
-                TreeNode* curr = q.front();
-                if (curr) {
-                    q.push(curr->left);
-                    q.push(curr->right);
-                }
-                v[i] = curr ? curr->val : INT_MIN;
-                q.pop();
-            }
-            for (int i = 0; i < size / 2; ++i) {
-                if (v[i] != v[size - 1 - i]) {
-                    return false;
-                }
-            }
+    bool isSymmetric(TreeNode* t1, TreeNode* t2) {
+        if (!t1 && !t2) {
+            return true;
         }
-        return true;
+        if (t1 && !t2 || t2 && !t1) {
+            return false;
+        }
+        return t1->val == t2->val && isSymmetric(t1->left, t2->right) &&
+               isSymmetric(t1->right, t2->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        return isSymmetric(root->left, root->right);
     }
 };
