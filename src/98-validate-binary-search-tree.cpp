89118cf7f9b0da4a17f8aad59d6c5e9662a75691
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-08 15:53:52 +0800

    98. 验证二叉搜索树
    给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
    
    有效 二叉搜索树定义如下：
    
    节点的左子树只包含 小于 当前节点的数。
    节点的右子树只包含 大于 当前节点的数。
    所有左子树和右子树自身必须也是二叉搜索树。

---
+++ src/98-validate-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool inorderTraversal(TreeNode* root, long long& prev) {
+        if (!root) {
+            return true;
+        }
+        bool left = inorderTraversal(root->left, prev);
+        if (root->val <= prev) {
+            return false;
+        }
+        prev = root->val;
+        bool right = inorderTraversal(root->right, prev);
+        return left && right;
+    }
+    bool isValidBST(TreeNode* root) {
+        long long prev = LONG_LONG_MIN;
+        return inorderTraversal(root, prev);
+    }
+};
