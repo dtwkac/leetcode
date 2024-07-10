a171380a904ce5faff325d021a6fedb3289b549b
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-10 14:03:49 +0800

    9. 回文数
    给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
    
    回文数
    是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
    
        例如，121 是回文，而 123 不是。

---
+++ src/9-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+        if (x % 10 == 0) {
+            return false;
+        }
+        int y = 0;
+        while (x >= y) {
+            if (x == y || x / 10 == y) {
+                return true;
+            }
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return false;
+    }
+};
