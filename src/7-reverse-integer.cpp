3e51d93bcb632ef582c4c854c0aa230495dce0fa
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-10 14:36:29 +0800

    7. 整数反转
    完善判断条件

--- src/7-reverse-integer.cpp
+++ src/7-reverse-integer.cpp
@@ -3,7 +3,12 @@ public:
     int reverse(int x) {
         int y = 0;
         while (x) {
-            if (y < INT_MIN / 10 || y > INT_MAX / 10) {
+            if (y < INT_MIN / 10 ||
+                (y == INT_MIN / 10 && x % 10 < INT_MIN % 10)) {
+                return 0;
+            }
+            if (y > INT_MAX / 10 ||
+                (y == INT_MAX / 10 && x % 10 > INT_MAX % 10)) {
                 return 0;
             }
             y = y * 10 + x % 10;
