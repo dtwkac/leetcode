35794f03bc1438dada33cf3ef2caf428cf040ce6
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-10 14:24:56 +0800

    8. 字符串转换整数 (atoi)
    改用 for 循环

--- src/8-string-to-integer-atoi.cpp
+++ src/8-string-to-integer-atoi.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if (s.empty()) {
-            return 0;
-        }
-        int i = 0;
-        while (s[i] == ' ') {
-            ++i;
-        }
+        int n = 0;
+        int size = s.size();
         bool positive = true;
-        if (s[i] == '-') {
-            positive = false;
-            ++i;
-        } else if (s[i] == '+') {
-            ++i;
-        }
-        while (s[i] == '0') {
-            ++i;
-        }
-        if (s[i] < '0' || s[i] > '9') {
-            return 0;
-        }
-        long long ans = 0;
-        for (int j = i; j < s.size() && s[j] >= '0' && s[j] <= '9'; ++j) {
-            ans = ans * 10 + s[j] - '0';
-            if (ans > INT_MAX) {
-                break;
+        for (int i = 0; i < size; ++i) {
+            if (s[i] == ' ') {
+                continue;
+            } else if (s[i] == '+') {
+                if (i < size - 1 && (s[i + 1] < '0' || s[i + 1] > '9')) {
+                    return 0;
+                }
+            } else if (s[i] == '-') {
+                if (i < size - 1 && (s[i + 1] < '0' || s[i + 1] > '9')) {
+                    return 0;
+                }
+                positive = false;
+            } else if (s[i] >= '0' && s[i] <= '9') {
+                if (n > INT_MAX / 10 ||
+                    (n == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
+                    return positive ? INT_MAX : INT_MIN;
+                }
+                n = n * 10 + (s[i] - '0');
+                if (i + 1 < size && (s[i + 1] < '0' || s[i + 1] > '9')) {
+                    break;
+                }
+            } else {
+                return 0;
             }
         }
-        ans = positive ? ans : -ans;
-        if (ans > INT_MAX) {
-            return INT_MAX;
-        }
-        if (ans < INT_MIN) {
-            return INT_MIN;
-        }
-        return ans;
-        return 0;
+        return positive ? n : -n;
     }
 };
