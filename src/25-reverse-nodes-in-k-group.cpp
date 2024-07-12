b993c0d0b781882fb66191558b5831267a9d1901
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-12 16:56:23 +0800

    25. K 个一组翻转链表
    给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
    
    k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
    
    你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。

---
+++ src/25-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode sentinel(-1, head), *front = &sentinel, *curr = head;
+        while (curr) {
+            ListNode *pioneer = curr, *new_head = nullptr, *new_tail = nullptr;
+            for (int i = 0; i < k; ++i) {
+                if (!pioneer) {
+                    return sentinel.next;
+                }
+                if (i == 0) {
+                    new_tail = pioneer;
+                }
+                if (i == k - 1) {
+                    new_head = pioneer;
+                }
+                pioneer = pioneer->next;
+            }
+            ListNode* prev = nullptr;
+            for (int i = 0; i < k; ++i) {
+                ListNode* post = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = post;
+            }
+            front->next = new_head;
+            new_tail->next = curr;
+            front = new_tail;
+        }
+        return sentinel.next;
+    }
+};
