81977064576c16336291fc57f958e66c6f6fd949
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-18 16:13:11 +0800

    88. 合并两个有序数组
    从后往前，从大到小排序，避免额外的拷贝

--- src/88-merge-sorted-array.cpp
+++ src/88-merge-sorted-array.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = m + n - 1; i >= n; --i) {
-            nums1[i] = nums1[i - n];
-        }
-        int i = 0, i1 = n, i2 = 0;
-        while (i1 < m + n || i2 < n) {
-            int n1 = i1 < m + n ? nums1[i1] : INT_MAX;
-            int n2 = i2 < n ? nums2[i2] : INT_MAX;
-            if (n1 < n2) {
-                nums1[i++] = n1;
-                ++i1;
+        int i1 = m - 1, i2 = n - 1, i = m + n - 1;
+        while (i1 > -1 || i2 > -1) {
+            int n1 = i1 > -1 ? nums1[i1] : INT_MIN;
+            int n2 = i2 > -1 ? nums2[i2] : INT_MIN;
+            if (n1 > n2) {
+                nums1[i--] = n1;
+                --i1;
             } else {
-                nums1[i++] = n2;
-                ++i2;
+                nums1[i--] = n2;
+                --i2;
             }
         }
     }
