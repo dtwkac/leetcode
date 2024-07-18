3fa6f718e9da0ad25f0dcdbe6e0d7e263c6567d7
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-18 15:10:50 +0800

    27. 移除元素
    用赋值替换 swap

--- src/27-remove-element.cpp
+++ src/27-remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int i = 0;
         for (int j = 0; j < nums.size(); ++j) {
             if (nums[j] != val) {
-                swap(nums[j], nums[i++]);
+                nums[i++] = nums[j];
             }
         }
         return i;
