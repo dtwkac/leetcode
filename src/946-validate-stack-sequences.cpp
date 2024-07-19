006bcaf515264848c555f25fc083f06f6626f4a0
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-19 11:15:17 +0800

    946. 验证栈序列
    改用基于范围的 for 循环

--- src/946-validate-stack-sequences.cpp
+++ src/946-validate-stack-sequences.cpp
@@ -3,8 +3,8 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int> s;
         int j = 0;
-        for (int i = 0; i < pushed.size(); ++i) {
-            s.push(pushed[i]);
+        for (int i : pushed) {
+            s.push(i);
             while (!s.empty() && s.top() == popped[j]) {
                 s.pop();
                 ++j;
