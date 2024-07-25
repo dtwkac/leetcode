cc4282107edb2f77c8bcd69874513f6b0bd91891
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-25 10:03:19 +0800

    119. 杨辉三角 II
    统一逻辑

--- src/119-pascals-triangle-ii.cpp
+++ src/119-pascals-triangle-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> v{1};
-        for (int i = 2; i <= rowIndex + 1; ++i) {
-            vector<int> curr(i, 1);
-            for (int j = 1; j < i - 1; ++j) {
-                curr[j] = v[j - 1] + v[j];
+        vector<int> v_1;
+        for (int i = 0; i <= rowIndex; ++i) {
+            vector<int> v(i + 1, 1);
+            for (int j = 1; j < i; ++j) {
+                v[j] = v_1[j - 1] + v_1[j];
             }
-            v = curr;
+            v_1 = v;
         }
-        return v;
+        return v_1;
     }
 };
