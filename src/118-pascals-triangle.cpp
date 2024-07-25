d0c59f6e421cd120cc03a256757ff45f872a7172
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-25 09:52:28 +0800

    118. 杨辉三角
    简化逻辑，省去不必要判断

--- src/118-pascals-triangle.cpp
+++ src/118-pascals-triangle.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> vv;
-        if (numRows < 1) {
-            return vv;
-        }
-        for (int i = 1; i <= numRows; ++i) {
-            vector<int> v(i, 1);
-            for (int j = 1; j < i - 1; ++j) {
-                v[j] = vv[i - 2][j - 1] + vv[i - 2][j];
+        vector<vector<int>> vv(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            vector<int> v(i + 1, 1);
+            for (int j = 1; j < i; ++j) {
+                v[j] = vv[i - 1][j - 1] + vv[i - 1][j];
             }
-            vv.push_back(v);
+            vv[i] = v;
         }
         return vv;
     }
