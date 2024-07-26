6e549d18ebc043cb1075d13931aafe9d4f24b35f
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-26 23:02:48 +0800

    111. 二叉树的最小深度
    改用层序遍历

--- src/111-minimum-depth-of-binary-tree.cpp
+++ src/111-minimum-depth-of-binary-tree.cpp
@@ -16,21 +16,26 @@ public:
         if (!root) {
             return 0;
         }
-        int min = INT_MAX;
-        function<void(TreeNode*, int)> dfs = [&dfs, &min](TreeNode* root,
-                                                          int depth) -> void {
-            if (!root) {
-                return;
-            }
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            int size = q.size();
             ++depth;
-            if (!root->left && !root->right) {
-                min = depth < min ? depth : min;
-                return;
+            for (int i = 0; i < size; ++i) {
+                TreeNode* t = q.front();
+                if (!t->left && !t->right) {
+                    return depth;
+                }
+                if (t->left) {
+                    q.push(t->left);
+                }
+                if (t->right) {
+                    q.push(t->right);
+                }
+                q.pop();
             }
-            dfs(root->left, depth);
-            dfs(root->right, depth);
-        };
-        dfs(root, 0);
-        return min;
+        }
+        return depth;
     }
 };
