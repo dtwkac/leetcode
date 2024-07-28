a878345e36e4421a6ee9f96c34654620c7cac95e
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-29 00:43:03 +0800

    257. 二叉树的所有路径
    将 path 改为按引用传递，显式删除回溯的节点

--- src/257-binary-tree-paths.cpp
+++ src/257-binary-tree-paths.cpp
@@ -13,23 +13,22 @@
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
+        string path;
         vector<string> paths;
-        function<void(TreeNode*, string)> dfs =
-            [&dfs, &paths](TreeNode* root, string path) -> void {
+        function<void(TreeNode*)> dfs = [&dfs, &path,
+                                         &paths](TreeNode* root) -> void {
             if (!root) {
                 return;
             }
-            path += to_string(root->val);
+            path += to_string(root->val) + "->";
             if (!root->left && !root->right) {
-                paths.push_back(path);
-                return;
+                paths.push_back(path.substr(0, path.size() - 2));
             }
-            path += "->";
-            dfs(root->left, path);
-            dfs(root->right, path);
+            dfs(root->left);
+            dfs(root->right);
+            path.erase(path.size() - to_string(root->val).size() - 2);
         };
-        string path;
-        dfs(root, path);
+        dfs(root);
         return paths;
     }
 };
