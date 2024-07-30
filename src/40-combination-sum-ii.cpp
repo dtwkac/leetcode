4f1000dd75b41cc31afc6dcc42f5b84df6123974
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-30 13:18:03 +0800

    40. 组合总和 II
    利用已排序特点，简化逻辑

--- src/40-combination-sum-ii.cpp
+++ src/40-combination-sum-ii.cpp
@@ -3,10 +3,9 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int> v;
         vector<vector<int>> vv;
-        vector<bool> selected(candidates.size(), false);
         function<void(const vector<int>&, int, int)> dfs =
-            [&dfs, &v, &vv, &selected](const vector<int>& candidates,
-                                       int target, int begin) -> void {
+            [&dfs, &v, &vv](const vector<int>& candidates, int target,
+                            int begin) -> void {
             if (target < 0) {
                 return;
             }
@@ -14,16 +13,13 @@ public:
                 vv.push_back(v);
                 return;
             }
-            unordered_set<int> s;
             for (int i = begin; i < candidates.size(); ++i) {
-                if (!selected[i] && s.find(candidates[i]) == s.cend()) {
-                    selected[i] = true;
-                    s.insert(candidates[i]);
-                    v.push_back(candidates[i]);
-                    dfs(candidates, target - candidates[i], i);
-                    v.pop_back();
-                    selected[i] = false;
+                if (i > begin && candidates[i] == candidates[i - 1]) {
+                    continue;
                 }
+                v.push_back(candidates[i]);
+                dfs(candidates, target - candidates[i], i + 1);
+                v.pop_back();
             }
         };
         sort(candidates.begin(), candidates.end());
