ac2d40c2ef71edd47db0fa51cbcb7b3c96604b69
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-31 14:48:37 +0800

    77. 组合
    给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
    
    你可以按 任何顺序 返回答案。

---
+++ src/77-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        vector<vector<int>> vv;
+        vector<bool> selected(n, false);
+        function<void(int, int, int)> dfs =
+            [&dfs, &v, &vv, &selected](int n, int k, int begin) -> void {
+            if (v.size() == k) {
+                vv.push_back(v);
+                return;
+            }
+            for (int i = begin; i <= n; ++i) {
+                if (!selected[i]) {
+                    v.push_back(i);
+                    selected[i] = true;
+                    dfs(n, k, i + 1);
+                    selected[i] = false;
+                    v.pop_back();
+                }
+            }
+        };
+        dfs(n, k, 1);
+        return vv;
+    }
+};
