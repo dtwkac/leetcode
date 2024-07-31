7ce4c4370c4299b4e0030ab8a58d71068e5d843c
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-07-31 13:23:50 +0800

    88. 合并两个有序数组
    改为更常见的写法

--- src/88-merge-sorted-array.cpp
+++ src/88-merge-sorted-array.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i1 = m - 1, i2 = n - 1, i = m + n - 1;
-        while (i1 > -1 || i2 > -1) {
-            int n1 = i1 > -1 ? nums1[i1] : INT_MIN;
-            int n2 = i2 > -1 ? nums2[i2] : INT_MIN;
-            if (n1 > n2) {
-                nums1[i--] = n1;
-                --i1;
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i > -1 && j > -1) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[i--] = n2;
-                --i2;
+                nums1[k--] = nums2[j--];
             }
+        }
+        while (i > -1) {
+            nums1[k--] = nums1[i--];
+        }
+        while (j > -1) {
+            nums1[k--] = nums2[j--];
         }
     }
 };
