9346ff71cef3a5790621d7cc1768e04e388d6638
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-08-04 08:44:58 +0800

    77. 组合
    防止 i = n 时 selected[i] 越界

--- src/77-combinations.cpp
+++ src/77-combinations.cpp
@@ -3,23 +3,23 @@ public:
     vector<vector<int>> combine(int n, int k) {
         vector<int> v;
         vector<vector<int>> vv;
-        vector<bool> selected(n, false);
+        vector<bool> selected(n + 1, false);
         function<void(int, int, int)> dfs =
-            [&dfs, &v, &vv, &selected](int n, int k, int begin) -> void {
-            if (v.size() == k) {
-                vv.push_back(v);
-                return;
-            }
-            for (int i = begin; i <= n; ++i) {
-                if (!selected[i]) {
-                    v.push_back(i);
-                    selected[i] = true;
-                    dfs(n, k, i + 1);
-                    selected[i] = false;
-                    v.pop_back();
+            [&dfs, &v, &vv, &selected](int n, int k, int begin) {
+                if (v.size() == k) {
+                    vv.push_back(v);
+                    return;
                 }
-            }
-        };
+                for (int i = begin; i <= n; ++i) {
+                    if (!selected[i]) {
+                        selected[i] = true;
+                        v.push_back(i);
+                        dfs(n, k, i + 1);
+                        v.pop_back();
+                        selected[i] = false;
+                    }
+                }
+            };
         dfs(n, k, 1);
         return vv;
     }
