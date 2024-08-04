064cbab380880b20e6f4144e6b66564847975ba2
dtwkac <143578277+dtwkac@users.noreply.github.com>
2024-08-04 08:52:41 +0800

    77. 组合
    添加 lambda 表达式的返回类型

--- src/77-combinations.cpp
+++ src/77-combinations.cpp
@@ -5,21 +5,21 @@ public:
         vector<vector<int>> vv;
         vector<bool> selected(n + 1, false);
         function<void(int, int, int)> dfs =
-            [&dfs, &v, &vv, &selected](int n, int k, int begin) {
-                if (v.size() == k) {
-                    vv.push_back(v);
-                    return;
+            [&dfs, &v, &vv, &selected](int n, int k, int begin) -> void {
+            if (v.size() == k) {
+                vv.push_back(v);
+                return;
+            }
+            for (int i = begin; i <= n; ++i) {
+                if (!selected[i]) {
+                    selected[i] = true;
+                    v.push_back(i);
+                    dfs(n, k, i + 1);
+                    v.pop_back();
+                    selected[i] = false;
                 }
-                for (int i = begin; i <= n; ++i) {
-                    if (!selected[i]) {
-                        selected[i] = true;
-                        v.push_back(i);
-                        dfs(n, k, i + 1);
-                        v.pop_back();
-                        selected[i] = false;
-                    }
-                }
-            };
+            }
+        };
         dfs(n, k, 1);
         return vv;
     }
